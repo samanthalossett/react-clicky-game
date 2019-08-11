4aef515086fa30d8a5a9b9e5eec9f6e4df2040eb
SamanthaLossett <samanthalossett@gmail.com>
2019-08-11 13:07:59 -0700

    made the components to function the rendering images, the click, and the count

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import data from './data';
 
-function App() {
+class App extends Component{
+  state={
+    imagestoclick: data,
+    count: 0,
+    clicked: []
+  }
+renderImages=() =>{
+  return this.state.imagestoclick.map(item => <img src={item.image} 
+  className="itemImage" 
+  onClick={this.countClicks}
+  value={item.id}
+  ></img>)
+}
+countClicks=(event) =>{
+  let id=event.currentTarget.getAttribute("value");
+  console.log(id);
+  if (this.state.clicked.includes(id)){
+    console.log("You've already clicked this one")
+  }
+  else {
+    this.setState({
+      count: this.state.count + 1,
+      clicked: this.state.clicked.concat(id)
+    })  
+  }
+}
+    render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>your current score is: {this.state.count}</h1>
+      {this.renderImages()}
+      {/* <ClickyGame imagestoclick={this.state.imagestoclick} /> */}
     </div>
   );
 }
-
+}
 export default App;
+
+
+
+
+// ./ current director 
+// .. go back one directory
---
+++ src/data.js
@@ -0,0 +1,19 @@
+let data=  [
+    {
+    id: 1,
+    image: 'https://cimg.tvgcdn.net/i/2017/04/25/c9421cf1-f8fb-44cd-a28f-eeaf422e09ce/c5e689d038ce148d5602e7b09d5462b3/170425-handmaidstale.jpg',
+    clicked: false
+    },
+    {
+      id: 2,
+      image: "https://s3.r29static.com//bin/entry/15e/0,0,2000,1050/x,80/2187582/theem-handmaids-taleem-new-2187582.jpg",
+      clicked: false
+      },
+      {
+        id: 3,
+        image: "https://s2.r29static.com//bin/entry/0bc/720x480,85/1783726/image.webp",
+        clicked: false
+        }
+  ]
+
+  module.exports= data;
