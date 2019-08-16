dd7c4186f547052336239407ea7985ba9ba4c412
SamanthaLossett <samanthalossett@gmail.com>
2019-08-15 18:17:33 -0700

    clicky game

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import data from './data';
 
-class App extends Component{
-  state={
+class App extends Component {
+  state = {
     imagestoclick: data,
     count: 0,
-    clicked: []
+    clicked: [],
+    highscore: 0
   }
-renderImages=() =>{
-  return this.state.imagestoclick.map(item => <img src={item.image} 
-  className="itemImage" 
-  onClick={this.countClicks}
-  value={item.id}
-  ></img>)
-}
-countClicks=(event) =>{
-  let id=event.currentTarget.getAttribute("value");
-  console.log(id);
-  if (this.state.clicked.includes(id)){
-    console.log("You've already clicked this one")
+  renderImages = () => {
+    return this.state.imagestoclick.map(item => <img src={item.image}
+      className="itemImage"
+      onClick={this.countClicks}
+      value={item.id}
+      key={item.id}
+    ></img>)
   }
-  else {
-    this.setState({
-      count: this.state.count + 1,
-      clicked: this.state.clicked.concat(id)
-    })  
+  countClicks = (event) => {
+    let id = event.currentTarget.getAttribute("value");
+    console.log(id);
+    if (this.state.clicked.includes(id)) {
+      alert("You've already clicked this one")
+    }
+    else {
+      this.setState({
+        count: this.state.count + 1,
+        clicked: this.state.clicked.concat(id)
+      })
+      this.state.imagestoclick.sort(() => Math.random() - 0.5)
+          return true; 
+        }
+        this.gameOver();
   }
-}
-    render(){
-  return (
-    <div className="App">
-      <h1>your current score is: {this.state.count}</h1>
-      {this.renderImages()}
-      {/* <ClickyGame imagestoclick={this.state.imagestoclick} /> */}
-    </div>
-  );
-}
+                            
+  gameOver= () =>{
+    if (this.state.count > this.state.highscore){
+      this.setState({highscore: this.state.count}, function(){
+        console.log(this.state.highscore, 
+          'huS');
+      });
+    }
+    console.log("Game Over! Your Score:", this.state.count);  
+    this.setState({count:0, clicked: []});
+    return true;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li>
+              
+              <h1>The "Handmaids" Clicky Game!</h1>
+            </li>
+            <li>
+            <h1>Your Score: {this.state.count}       High Score: {this.state.highscore}
+            </h1>
+            </li>
+          </ul>
+        </nav>
+        <br></br>
+        <header className="header">
+          <h2>Click an image to begin but don't click the same image twice!</h2>
+          
+        </header>
+       
+        {this.renderImages()}
+      
+      </div>
+    );
+  }
 }
 export default App;
 
--- src/data.js
+++ src/data.js
@@ -13,7 +13,53 @@ let data=  [
         id: 3,
         image: "https://s2.r29static.com//bin/entry/0bc/720x480,85/1783726/image.webp",
         clicked: false
-        }
+        },
+        {
+            id: 4,
+            image: 'https://www.sbs.com.au/guide/sites/sbs.com.au.guide/files/styles/full/public/tht_110_0.jpg?itok=5uB3yNjL',
+            clicked: false
+            },
+            {
+                id: 5,
+                image: 'https://www.stylist.co.uk/images/app/uploads/2017/10/25102649/3f80a5adae47caa67145ac5ce4b8c7cf-827x551.jpg?w=1200&h=1&fit=max&auto=format%2Ccompress',
+                clicked: false
+                },
+                {
+                    id: 6,
+                    image: 'https://media.vanityfair.com/photos/592f2c9f42539b38d20a65fe/16:9/w_1280,c_limit/Max-Minghella.jpg',
+                    clicked: false
+                    },
+                    {
+                        id: 7,
+                        image: 'https://pmctvline2.files.wordpress.com/2019/07/the-handmaids-tale-recap-season-3-episode-8-unfit-1.jpg?w=620&h=420&crop=1',
+                        clicked: false
+                        },
+                        {
+                            id: 8,
+                            image: 'https://cimg.tvgcdn.net/i/2019/06/06/59b49e32-30a3-4ce6-97e7-52cb7976ebd4/190606-elisabeth-moss-bradley-whitford-the-handmaids-tale.jpg',
+                            clicked: false
+                            },
+                            {
+                                id: 9,
+                                image: 'https://s.yimg.com/ny/api/res/1.2/NcmUC_bHwr579IszDgELBQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9NTM4/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/6e0f82f2cd76b361ce76b81ccc0bbbc8',
+                                clicked: false
+                                },
+                                {
+                                    id: 10,
+                                    image: 'https://fsmedia.imgix.net/2e/0b/7c/ba/176f/4ad2/8f5e/35292839a945/alexis-bledel-as-ofglen-in-the-handmaids-tale.jpeg',
+                                    clicked: false
+                                    },
+                                    {
+                                        id: 11,
+                                        image: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/handmaids-moira.jpg',
+                                        clicked: false
+                                        },
+                                        {
+                                            id: 12,
+                                            image: 'https://pisco.meaww.com/f9b8a16b-02f9-40ca-ba16-d097d5eb9bea.png',
+                                            clicked: false
+                                            },
+
   ]
 
   module.exports= data;
